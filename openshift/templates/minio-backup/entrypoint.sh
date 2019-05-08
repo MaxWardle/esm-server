a7c0c619dc534b92073d0f94c47afd1ebaef5db9
Max Wardle <max.wardle@gov.bc.ca>
2019-05-08 14:35:35 -0700

    EM-325: Added backup scripts

--- openshift/templates/minio-backup/entrypoint.sh
+++ openshift/templates/minio-backup/entrypoint.sh
@@ -1,61 +1,52 @@
 #!/bin/sh
 
-curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Minio Test"}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
+set -euxo pipefail
 
-# set -euo pipefail
-# IFS=$'\n\t'
-# [ "${VERBOSE:-}" != true ]|| set -x
-
-# # PVC mount and folder variables, removing any trailing slashes (%/)
-# #
-# SRC_MNT=${SRC_MNT:-/mnt/source}
-# DEST_MNT=${DEST_MNT:-/backup}
-# SRC_MNT=${SRC_MNT%/}
-# DEST_MNT=${DEST_MNT%/}
-# #
-# DEST_DIR=${DEST_MNT}/documents
-# TMP_BK=${NEW_BK:-${DEST_DIR}/bk-tmp}
-# PRV_BK=${PRV_BK:-${DEST_DIR}/bk-prev}
-# NEW_BK=${NEW_BK:-${DEST_DIR}/bk}
+# PVC mount and folder variables, removing any trailing slashes (%/)
+#
+SRC_MNT=${SRC_MNT:-/mnt/source}
+DEST_MNT=${DEST_MNT:-/mnt/dest}
+SRC_MNT=${SRC_MNT%/}
+DEST_MNT=${DEST_MNT%/}
 
 
-# # Drop to one previous backup
-# # Either directory does not exist, or remove directory.
-# [ ! -d ${PRV_BK} ]|| rm -rf ${PRV_BK}
+# Copy and verify
+#
+if ! restic -r ${DEST_MNT}/backups/ check
+then
+	echo "Copy failed!  Previous backups retained."
+  curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Backup for EPIC Minio files FAILURE! Backups are paused until the issue is addressed. Please see documentation: "}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
+	exit 1
+fi
+
+#check disk usage on backup pod here
 
 
-# # Copy and verify
-# #
-# mkdir -p ${TMP_BK}
-# if ! rsync -avh ${SRC_MNT}/ ${TMP_BK}/
-# then
-# 	echo "Copy failed!  Previous backups retained."
-# 	rm -rf ${TMP_BK}
-# 	exit 1
-# fi
+# Check if NFS repository is initialized.  If not, initialize it.
+# RESTIC_PASSWORD is required.
+if ! restic -r ${DEST_MNT}/backups/ snapshots
+then
+  restic -r ${DEST_MNT}/backups/ init
+fi
 
+# Backup files using delta (de-duplicate) and encryption
+restic --cache-dir ${DEST_DIR}/.cache -r ${DEST_MNT}/backups/ backup ${SRC_MNT}
 
-# # Shuffle and show disk usage
-# # Either directory doesn't exist, or move it
-# [ ! -d ${NEW_BK} ]|| mv ${NEW_BK} ${PRV_BK}
-# mv ${TMP_BK} ${NEW_BK}
-# du -hd 1 ${DEST_MNT}
+# check repository integrity before exiting
+if ! restic -r ${DEST_MNT}/backups/ check
+then
+	echo "Copy failed!  Previous backups retained."
+  curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Backup for EPIC Minio files FAILURE! Backups are paused until the issue is addressed. Please see documentation: "}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
+	exit 1
+else
 
+  # Clean up old snapshots.
+  # As an example, the following arguments:
+  # --keep-last 5 --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 2
+  # will keep the most recent 5 due to the size of the folder and space limitations on openshift
+  # The rest will be pruned.
+  restic -r ${DEST_MNT}/backups/ forget --keep-last 5 --prune
 
-# # Check if NFS repository is initialized.  If not, initialize it.
-# # RESTIC_PASSWORD is required.
-# if ! restic -r /mnt/dest/gwells-documents snapshots > /dev/null 2>&1; then
-#     restic -r /mnt/dest/gwells-documents init ; fi
-
-# # Backup files using delta (de-duplicate) and encryption
-# restic --cache-dir ${DEST_DIR}/.cache -r /mnt/dest/gwells-documents backup ${NEW_BK}
-
-# # Clean up old snapshots.
-# # As an example, the following arguments:
-# # --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 2
-# # will keep the most recent 7 daily snapshots, 5 weekly, 12 monthly, and 2 yearly snapshots.
-# # The rest will be pruned.
-# restic -r /mnt/dest/gwells-documents forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 10 --prune
-
-# # check repository integrity before exiting
-# restic -r /mnt/dest/gwells-documents check
+  echo "Backup Success!"
+  curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Backup for EPIC Minio files SUCCESS!"}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
+fi
