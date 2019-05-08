dd06fc2b1cfeb1a6a6af508dc4fb5c315fe3a70f
Max Wardle <max.wardle@gov.bc.ca>
2019-05-08 15:04:02 -0700

    EM-325: updated backup script

--- openshift/templates/minio-backup/entrypoint.sh
+++ openshift/templates/minio-backup/entrypoint.sh
@@ -11,15 +11,6 @@ DEST_MNT=${DEST_MNT%/}
 
 
 # Copy and verify
-#
-if ! restic -r ${DEST_MNT}/backups/ check
-then
-	echo "Copy failed!  Previous backups retained."
-  curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Backup for EPIC Minio files FAILURE! Backups are paused until the issue is addressed. Please see documentation: "}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
-	exit 1
-fi
-
-#check disk usage on backup pod here
 
 
 # Check if NFS repository is initialized.  If not, initialize it.
@@ -28,6 +19,16 @@ if ! restic -r ${DEST_MNT}/backups/ snapshots
 then
   restic -r ${DEST_MNT}/backups/ init
 fi
+
+# check integrity of previous backups
+if ! restic -r ${DEST_MNT}/backups/ check
+then
+	echo "Copy failed!  Previous backups retained."
+  curl -X POST -H 'Content-Type: application/json' --data '{"username":"BakBot","icon_emoji":":robot:","text":"Backup for EPIC Minio files FAILURE! Backups are paused until the issue is addressed. Please see documentation: "}' https://chat.pathfinder.gov.bc.ca/hooks/BTqW65kZWWrs3TnDt/Yz9QocenFmpuWyqr3Xapgo9c3X4wm8RBfdnizej7gqeBqfN3
+	exit 1
+fi
+
+# check disk usage on backup pod here
 
 # Backup files using delta (de-duplicate) and encryption
 restic --cache-dir ${DEST_DIR}/.cache -r ${DEST_MNT}/backups/ backup ${SRC_MNT}
